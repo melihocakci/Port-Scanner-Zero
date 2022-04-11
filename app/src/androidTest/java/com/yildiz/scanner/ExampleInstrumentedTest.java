347ec517e9acddec3db4927c9d095ed9a2e3d142
Melih Ocakcı <melihocakci@gmail.com>
2022-04-11 16:30:16 +0300

    changed project name

--- app/src/androidTest/java/com/yildiz/scanner/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/yildiz/scanner/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.android.test", appContext.getPackageName());
+        assertEquals("com.yildiz.scanner", appContext.getPackageName());
     }
 }
