92ef28630850c2f425494b90c7d857c6491a9064
Melih Ocakcı <melihocakci@gmail.com>
2022-05-23 03:45:08 +0300

    created local class

--- app/src/main/java/com/yildiz/scanner/ScanActivity.java
+++ app/src/main/java/com/yildiz/scanner/ScanActivity.java
@@ -18,10 +18,77 @@ public class ScanActivity extends AppCompatActivity {
     private EditText port_field;
     private TextView output_field;
     private double start;
-    private int portNum;
-    private LinkedList<Integer> openPorts;
     private boolean scanning;
     private final Handler handler = new Handler();
+
+    private class ScanHandler implements Runnable {
+        @Override
+        public void run() {
+            // get host address from text field
+            String hostname = ip_field.getText().toString();
+            // get port list from text field
+            String port_input = port_field.getText().toString();
+
+            // get host address
+            InetAddress host;
+            try {
+                host = InetAddress.getByName(hostname);
+            } catch (Exception e) {
+                print("Cannot get host address");
+                scanning = false;
+                return;
+            }
+
+            // get port list
+            LinkedList<Integer> portList = new LinkedList<Integer>();
+            try {
+                String[] ports = port_input.split(",");
+                for(String i : ports) {
+                    if(i.contains("-")) {
+                        String[] gap = i.split("-");
+                        int first = Integer.parseInt(gap[0]);
+                        int last = Integer.parseInt(gap[1]);
+                        for(int j = first; j <= last; j++) {
+                            portList.add(j);
+                        }
+                    } else {
+                        portList.add(Integer.parseInt(i));
+                    }
+                }
+            } catch (Exception e) {
+                print("Invalid port input");
+                scanning = false;
+                return;
+            }
+
+            int portNum = portList.size();
+            LinkedList<Integer> openPorts = Scanner.scanPorts(host, portList);
+
+            if(scanning) {
+                scanning = false;
+                // end timer
+                double end = System.currentTimeMillis();
+                StringBuilder output = new StringBuilder();
+                output.append("Scan completed.\n");
+                output.append("Time elapsed: ").append((end - start) / 1000).append("s\n");
+                output.append(portNum - openPorts.size()).append(" closed ports\n");
+                for(int i = 0; i < openPorts.size(); i++) {
+                    output.append("Port ").append(openPorts.get(i)).append(" is open\n");
+                }
+
+                print(output.toString());
+            }
+        }
+
+        public void print(String str) {
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    output_field.setText(str);
+                }
+            });
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,88 +104,22 @@ public class ScanActivity extends AppCompatActivity {
         if(scanning) {
             scanning = false;
             Scanner.stopScan();
-            print("Scan stopped");
-            return;
+            output_field.setText("Scan stopped");
+        } else {
+            // started scan
+            scanning = true;
+            // start timer
+            start = System.currentTimeMillis();
+            // close keyboard
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
+            Runnable scanHandler = new ScanHandler();
+
+            Thread thread = new Thread(scanHandler);
+            thread.start();
+
+            output_field.setText("Scanning");
         }
-
-        // started scan
-        scanning = true;
-        // start timer
-        start = System.currentTimeMillis();
-        // close keyboard
-        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                // get host address from text field
-                String hostname = ip_field.getText().toString();
-                // get port list from text field
-                String port_input = port_field.getText().toString();
-
-                // get host address
-                InetAddress host;
-                try {
-                    host = InetAddress.getByName(hostname);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    return;
-                }
-
-                // get port list
-                LinkedList<Integer> portList = new LinkedList<Integer>();
-                try {
-                    String[] ports = port_input.split(",");
-                    for(String i : ports) {
-                        if(i.contains("-")) {
-                            String[] gap = i.split("-");
-                            int first = Integer.parseInt(gap[0]);
-                            int last = Integer.parseInt(gap[1]);
-                            for(int j = first; j <= last; j++) {
-                                portList.add(j);
-                            }
-                        } else {
-                            portList.add(Integer.parseInt(i));
-                        }
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    return;
-                }
-
-                portNum = portList.size();
-                openPorts = Scanner.scanPorts(host, portList);
-
-                if(scanning) {
-                    // end timer
-                    double end = System.currentTimeMillis();
-                    StringBuilder output = new StringBuilder();
-                    output.append("Scan completed.\n");
-                    output.append("Time elapsed: ").append((end - start) / 1000).append("s\n");
-                    output.append(portNum - openPorts.size()).append(" closed ports\n");
-                    for(int i = 0; i < openPorts.size(); i++) {
-                        output.append("Port ").append(openPorts.get(i)).append(" is open\n");
-                    }
-
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            print(output.toString());
-                        }
-                    });
-                    scanning = false;
-                }
-            }
-        };
-
-        Thread scanHandler = new Thread(runnable);
-        scanHandler.start();
-
-        output_field.setText("Scanning..");
-    }
-
-    public void print(String text) {
-        output_field.setText(text);
     }
 }
