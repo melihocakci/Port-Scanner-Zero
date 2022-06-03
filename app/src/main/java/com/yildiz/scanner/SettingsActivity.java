90176062cc3bd2d9477b34ea55d76e04dce4e272
KBEvcil <l1119096@std.yildiz.edu.tr>
2022-06-03 20:47:05 +0300

    progress bar ve spinner eklendi

--- app/src/main/java/com/yildiz/scanner/SettingsActivity.java
+++ app/src/main/java/com/yildiz/scanner/SettingsActivity.java
@@ -10,5 +10,8 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        getSupportActionBar().setTitle("Settings");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
